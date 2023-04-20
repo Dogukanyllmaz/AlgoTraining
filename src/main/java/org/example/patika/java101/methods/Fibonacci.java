3a0dcf9a0ebc9decf1a84d19fd21585ca23c7d8e
Dogu_ <dogu_rog@hotmail.com>
2023-04-20 22:09:03 +0300

    fibonacci pseudo

---
+++ src/main/java/org/example/patika/java101/methods/Fibonacci.java
@@ -0,0 +1,27 @@
+package org.example.patika.java101.methods;
+
+/**
+ * patika java 101
+ *
+ * @author H. Doğukan Yılmaz
+ */
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        /*  1 1 2 3 5 8 ...
+            f(1)=1
+            f(2)=1
+            f(n)=f(n-1)+f(n-2)
+            f(3)=f(2)+f(1)
+            f(4)=f(3)+f(2)
+            f(5)=f(4)+f(3)
+               .
+               .
+               .    */
+
+
+
+
+
+    }
+}
