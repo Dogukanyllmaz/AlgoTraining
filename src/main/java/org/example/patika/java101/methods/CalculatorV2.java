bc444895bd10da30684b3c7e6cf8afa345a5b5dd
Dogu_ <dogu_rog@hotmail.com>
2023-04-20 23:07:01 +0300

    Calculator 0.0.2V

---
+++ src/main/java/org/example/patika/java101/methods/CalculatorV2.java
@@ -0,0 +1,16 @@
+package org.example.patika.java101.methods;
+
+/**
+ * Calculator
+ * 0.0.2V
+ * @author Hç Doğukan Yılmaz
+ */
+public class CalculatorV2 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
